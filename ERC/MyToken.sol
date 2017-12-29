58d5036de37c668eb312470aeffb2d9f1e19e2f9
Sean Wilkinson <swilkinson@smarttermlms.com>
2017-12-28 22:53:40 -0500

    Add and subtract using OZ SafeMath

--- ERC/MyToken.sol
+++ ERC/MyToken.sol
@@ -4,27 +4,30 @@ import "./Token.sol";
 import "./ERC20.sol";
 import "./ERC223.sol";
 import "./ERC223ReceivingContract.sol";
+import "github.com/OpenZeppelin/zeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract MyFirstToken is Token("MFT", "My First Token", 18, 1000), ERC20, ERC223 {
+
+    using SafeMath for uint;
 
     function MyFirstToken() public {
         _balanceOf[msg.sender] = _totalSupply;
     }
-    
+
     function totalSupply() public constant returns (uint) {
         return _totalSupply;
     }
-    
+
     function balanceOf(address _addr) public constant returns (uint) {
         return _balanceOf[_addr];
     }
 
     function transfer(address _to, uint _value) public returns (bool) {
-        if (_value > 0 && 
+        if (_value > 0 &&
             _value <= _balanceOf[msg.sender] &&
             !isContract(_to)) {
-            _balanceOf[msg.sender] -= _value;
-            _balanceOf[_to] += _value;
+            _balanceOf[msg.sender] =_balanceOf[msg.sender].sub(_value);
+            _balanceOf[_to] = _balanceOf[_to].add(_value);
             Transfer(msg.sender, _to, _value);
             return true;
         }
@@ -32,12 +35,12 @@ contract MyFirstToken is Token("MFT", "My First Token", 18, 1000), ERC20, ERC223
     }
 
     function transfer(address _to, uint _value, bytes _data) public returns (bool) {
-        if (_value > 0 && 
+        if (_value > 0 &&
             _value <= _balanceOf[msg.sender] &&
             isContract(_to)) {
-            _balanceOf[msg.sender] -= _value;
-            _balanceOf[_to] += _value;
-            ERC223ReceivingContract _contract = ERC223ReceivingContract(_to);
+            _balanceOf[msg.sender] = _balanceOf[msg.sender].sub(_value);
+            _balanceOf[_to] = _balanceOf[_to].add(_value);
+            ERC223ReceivingsolsasdasdasContract _contract = ERC223ReceivingContract(_to);
             _contract.tokenFallback(msg.sender, _value, _data);
             Transfer(msg.sender, _to, _value, _data);
             return true;
@@ -45,7 +48,7 @@ contract MyFirstToken is Token("MFT", "My First Token", 18, 1000), ERC20, ERC223
         return false;
     }
 
-    function isContract(address _addr) returns (bool) {
+    function isContract(address _addr) private constant returns (bool) {
         uint codeSize;
         assembly {
             codeSize := extcodesize(_addr)
@@ -58,21 +61,21 @@ contract MyFirstToken is Token("MFT", "My First Token", 18, 1000), ERC20, ERC223
             _value > 0 &&
             _allowances[_from][msg.sender] >= _value &&
             _balanceOf[_from] >= _value) {
-            _balanceOf[_from] -= _value;
-            _balanceOf[_to] += _value;
+            _balanceOf[_from] = _balanceOf[_from].sub(_value);
+            _balanceOf[_to] = _balanceOf[_to].add(_value);
             _allowances[_from][msg.sender] -= _value;
             Transfer(_from, _to, _value);
             return true;
         }
         return false;
     }
-    
+
     function approve(address _spender, uint _value) public returns (bool) {
         _allowances[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
-    
+
     function allowance(address _owner, address _spender) public constant returns (uint) {
         return _allowances[_owner][_spender];
     }
